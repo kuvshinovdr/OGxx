39460e56fcfa742a997602ede47cc006fdc018f1
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-10-12 15:16:47 +0500

    Introducing matrix.hpp
    Introducing matrix.hpp in include directory and dense_matrix.cpp in source directory.

---
+++ include/matrix.hpp
@@ -0,0 +1,15 @@
+/// @file matrix.hpp
+/// @brief Abstract matrix interface
+/// @author Kuvshinov D.R.
+#ifndef OGXX_MATRIX_HPP_INCLUDED
+#define OGXX_MATRIX_HPP_INCLUDED
+
+/// Root namespace of the OGxx library.
+namespace ogxx
+{
+
+
+
+}
+
+#endif//OGXX_MATRIX_HPP_INCLUDED
---
+++ source/dense_matrix.cpp
@@ -0,0 +1,11 @@
+/// @file dense_matrix.cpp
+/// @brief Dense matrix implementation (of abstract matrix interface from matrix.hpp).
+/// @author Kuvshinov D.R.
+#include "matrix.hpp"
+
+namespace ogxx
+{
+
+
+
+}
