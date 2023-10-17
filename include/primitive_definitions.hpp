bbd1571cfd2cf9d25b70de2763b886ad87f99fc9
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-10-17 16:14:27 +0500

    Update primitive_definitions.hpp
    Added generic clamp, min, max functions.

--- include/primitive_definitions.hpp
+++ include/primitive_definitions.hpp
@@ -7,6 +7,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <limits>
+#include <type_traits>
 #include <memory>
 
 
@@ -40,7 +41,7 @@ namespace ogxx
   /////////////////////////////////////////////////////////////////////////////
   // Functions
 
-  /// @brief Check if a value is within a segment with the specified bounds
+  /// @brief Check if a value is within a segment with the specified bounds, uses operator<=
   /// @param value the value to check
   /// @param lower the lower bound for the value
   /// @param upper the upper bound for the value
@@ -52,11 +53,68 @@ namespace ogxx
     return lower <= value && value <= upper;
   }
 
-  // max
 
-  // min
+  /// @brief Clamp a value into [lower, upper] segment, uses operator<
+  /// @tparam Val value type
+  /// @tparam Lo lower bound type
+  /// @tparam Up upper bound type
+  /// @param value the value to be clamped
+  /// @param lower the lower bound
+  /// @param upper the upper bound
+  /// @return lower if value < lower, upper if upper < value, value if it is within [lower, upper]
+  template <typename Val, typename Lo, typename Up>
+  [[nodiscard]] constexpr auto clamp(Val&& value, Lo&& lower, Up&& upper)
+    -> std::common_type_t<Val, Lo, Up>
+  {
+    if (value < lower)
+      return std::forward<Lo>(lower);
+    if (upper < value)
+      return std::forward<Up>(upper);
+    return std::forward<Val>(value);
+  }
 
-  // clamp
+
+  template <typename First, typename Second>
+  [[nodiscard]] constexpr auto max(First&& first, Second&& second)
+    -> std::common_type_t<First, Second>
+  {
+    if (first < second)
+      return std::forward<Second>(second);
+    return std::forward<First>(first);
+  }
+
+
+  template <typename A1, typename A2, typename... Args>
+  [[nodiscard]] constexpr auto max(A1&& a1, A2&& a2, Args&&... args)
+    -> std::common_type_t<A1, A2, Args...>
+  {
+    if constexpr (sizeof...(Args) == 1)
+      return max(max(std::forward<A1>(a1), std::forward<A2>(a2)), std::forward<Args>(args));
+    else
+      return max(max(std::forward<A1>(a1), std::forward<A2>(a2)), max(std::forward<Args>(args)));
+  }
+
+
+
+  template <typename First, typename Second>
+  [[nodiscard]] constexpr auto min(First&& first, Second&& second)
+    -> std::common_type_t<First, Second>
+  {
+    if (second < first)
+      return std::forward<Second>(second);
+    return std::forward<First>(first);
+  }
+
+
+  template <typename A1, typename A2, typename... Args>
+  [[nodiscard]] constexpr auto min(A1&& a1, A2&& a2, Args&&... args)
+    -> std::common_type_t<A1, A2, Args...>
+  {
+    if constexpr (sizeof...(Args) == 1)
+      return min(min(std::forward<A1>(a1), std::forward<A2>(a2)), std::forward<Args>(args));
+    else
+      return min(min(std::forward<A1>(a1), std::forward<A2>(a2)), min(std::forward<Args>(args)));
+  }
 
 }
 
