a4e16981b7a02c22a5140f26ce737afccdb44514
ChernyshevMikhail <145912899+ChernyshevMikhail@users.noreply.github.com>
2023-10-21 23:19:18 +0500

    Pm2 chernyshev m v (#9)
    * создал файл stl...
    
    * написал программу
    
    * работаю над задачей
    
    * A bit of polishing
    
    Stl_iterator polishing before merging into main.
    
    ---------
    
    Co-authored-by: Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>

---
+++ source/stl_iterator.hpp
@@ -0,0 +1,28 @@
+/// @file stl_iterator.hpp
+/// @brief This file defines the Stl_iterator class, which provides an iterator for standard C++ containers.
+/// @author ChernyshevMV
+#ifndef OGXX_STL_ITERATOR_HPP
+#define OGXX_STL_ITERATOR_HPP
+
+#include <iterator>
+#include <ogxx/iterator.hpp>
+
+namespace ogxx {
+template <typename T, typename It, typename Sent>
+class Stl_iterator : public Basic_iterator<T> {
+private:
+    It current;
+    Sent end;
+public:
+    Stl_iterator(It a, Sent b) : current(a), end(b) {}   
+    template <typename Cont>
+    Stl_iterator(Cont&& a) : current(std::begin(a)), end(std::end(a)) {}
+    bool next(T& out_item) noexcept override {
+        if (current == end) return false;
+        out_item = *current;
+        ++current;
+        return true;
+    }
+};
+}  
+#endif
