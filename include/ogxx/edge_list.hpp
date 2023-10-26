d138d47ccbb3802576ec5c52fa52fa59449b3e82
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-10-27 03:05:57 +0500

    Created Graph_view adapter
    Graph_view is intended to be graph representation conversion mediator.

--- include/ogxx/edge_list.hpp
+++ include/ogxx/edge_list.hpp
@@ -4,25 +4,12 @@
 #ifndef OGXX_EDGE_LIST_HPP_INCLUDED
 #define OGXX_EDGE_LIST_HPP_INCLUDED
 
-#include "primitive_definitions.hpp"
-#include "iterable.hpp"
+#include "vertex_pair.hpp"
 
 
 /// Root namespace of the OGxx library.
 namespace ogxx
 {
-
-  /// @brief Unique identifier of a vertex within a graph.
-  using Vertex_index = Scalar_index;
-
-  /// @brief Edge of a graph may be represented by a pair of vertices.
-  using Vertex_pair = std::pair<Vertex_index, Vertex_index>;
-
-  /// @brief Iterator object for enumerating a vertex pair (edge) list.
-  using Vertex_pair_iterator = Basic_iterator<Vertex_pair>;
-
-  /// @brief An owning pointer to a Vertex_pair_iterator object.
-  using Vertex_pair_iterator_uptr = std::unique_ptr<Vertex_pair_iterator>;
 
   /// @brief Interface (abstract base class) for storing an edge list of a graph.
   /// Indexed_iterable of Vertex_pair, throws on out-of-range?
---
+++ include/ogxx/graph_view.hpp
@@ -0,0 +1,98 @@
+/// @file graph_view.hpp
+/// @brief Graph view interface that may be used for graph representation coversion.
+/// @author Kuvshinov D.R. kuvshinovdr at yandex.ru
+#ifndef OGXX_GRAPH_VIEW_HPP_INCLUDED
+#define OGXX_GRAPH_VIEW_HPP_INCLUDED
+
+#include "vertex_pair.hpp"
+
+
+/// Root namespace of the OGxx library.
+namespace ogxx
+{
+
+  class Graph_view
+  {
+  public:
+    virtual ~Graph_view() {}
+
+    // Constant interface
+
+    /// Get the count of vertices in the graph, vertex indices are 0, 1, ..., vertex_count() - 1.
+    [[nodiscard]] virtual auto vertex_count() const noexcept
+      -> Scalar_size = 0;
+
+    /// Get the count of edges in the graph, edge indices are 0, 1, ..., edge_count() - 1.
+    /// The iterator returned by iterate_edges() should make edge_count() steps.
+    [[nodiscard]] virtual auto edge_count() const noexcept
+      -> Scalar_size = 0;
+
+    /// @brief Iterate through edges represented by vertex index pairs.
+    /// Iterator should make edge_count() steps.
+    /// @return iterator object iterating through (Vertex_index, Vertex_index) pairs.
+    [[nodiscard]] virtual auto iterate_edges() const
+      -> Vertex_pair_iterator_uptr = 0;
+
+    // Non-constant interface
+
+    /// @brief Provide the would-be vertex count. May be used to reserve memory in advance.
+    /// @param count how many vertices the graph should contain
+    virtual void set_vertex_count(Scalar_size count) = 0;
+
+    /// @brief Provide the would-be edge count. May be used to reserve memory in advance.
+    /// @param count how many edges the graph should contain, aftermath edge_count() call may return another value
+    virtual void set_edge_count(Scalar_size count) = 0;
+
+    /// @brief Connect the vertices of the pair edge.
+    /// @param edge (first, second) pair of vertex indices
+    /// @return true if edge has been added, false otherwise (e.g. the edge already exists)
+    virtual auto connect(Vertex_pair edge)
+      -> bool = 0;
+
+    /// @brief Add edge from -> to.
+    /// @param from vertex index for the outcoming vertex
+    /// @param to vertex index for the incoming vertex
+    /// @return true if edge has been added, false otherwise (e.g. the edge already exists)
+    auto connect(Vertex_index from, Vertex_index to)
+      -> bool { return connect(Vertex_pair{ from, to }); }
+
+    /// @brief Disconnect the vertices of the pair edge
+    /// @param edge (first, second) pair of vertex indices
+    /// @return true if edge has been removed, false otherwise (e.g. there was no such edge)
+    virtual auto disconnect(Vertex_pair edge)
+      -> bool = 0;
+
+    /// @brief Remove edge from -> to.
+    /// @param from vertex index for the outcoming vertex
+    /// @param to vertex index for the incoming vertex
+    /// @return true if edge has been removed, false otherwise (e.g. there was no such edge) 
+    auto disconnect(Vertex_index from, Vertex_index to)
+      -> bool { return disconnect(Vertex_pair{ from, to }); }
+
+    /// @brief Copy all edges of gv into this graph.
+    /// @param gv source graph object
+    /// @return how many edges of gv have been inserted into this graph
+    auto copy_from(Graph_view const& gv)
+      -> Scalar_size
+    {
+      Scalar_size added_edges = 0;
+      Scalar_size const verts = max(vertex_count(), gv.vertex_count());
+      set_vertex_count(verts);
+      set_edge_count(edge_count() + gv.edge_count());
+
+      auto gv_edge_iter = gv.iterate_edges();
+      for (Vertex_pair vp; gv_edge_iter->next(vp);)
+        if (connect(vp))
+          ++added_edges;
+
+      return added_edges;
+    }
+
+  protected:
+    Graph_view& operator=(Graph_view const&) = default;
+    Graph_view& operator=(Graph_view&&) = default;
+  };
+
+}
+
+#endif//OGXX_GRAPH_VIEW_HPP_INCLUDED
