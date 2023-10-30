e532f880fcb4d4cfc39979d6e1f0cb4d6060147d
KonstantinPM201 <K.S.Martianov@urfu.me>
2023-10-30 11:24:18 +0500

    Установил себе doctest

---
+++ tests/symmetric_dense_st_matrix_test.cpp
@@ -0,0 +1,24 @@
+/// @file symmetric_dense_st_matrix_test.cpp
+/// @brief Tests for Symmetric_dense_st_matrix.
+/// @author Martianov K.S
+#include "testing_head.hpp"
+#include <../source/symmetric_dense_st_matrix.cpp>
+
+/*
+TEST_SUITE("Initialization")
+{
+  TEST_CASE("Initialization")
+  {
+    Symmetric_dense_st_matrix A(5);
+  }
+
+ 
+}
+
+*/
+namespace ogxx
+{
+int main(){
+    Symmetric_dense_st_matrix<int> A(5);
+}
+}
