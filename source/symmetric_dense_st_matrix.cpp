10982c961a2bad0c558401ac20855b3e8edea397
KonstantinPM201 <K.S.Martianov@urfu.me>
2023-10-30 16:14:35 +0500

    Добавил заглушки

--- source/symmetric_dense_st_matrix.cpp
+++ source/symmetric_dense_st_matrix.cpp
@@ -9,11 +9,11 @@ namespace ogxx
   class Symmetric_dense_st_matrix : public St_matrix<ST>
   {
   private:
-    std::vector<ST> data;
-    size_t size;
+    std::vector<ST> data_;
+    size_t size_;
 
   public:
-    Symmetric_dense_st_matrix(size_t size): data(size * (size + 1) / 2, ST{}), size(size)
+    Symmetric_dense_st_matrix(size_t size): data_(size * (size + 1) / 2, ST{}), size_(size)
     {
       if (size == 0)
         throw std::invalid_argument("Matrix size must be greater than 0.");
@@ -22,7 +22,7 @@ namespace ogxx
     // Метод Shape
     [[nodiscard]] Matrix_shape shape() const noexcept override
     {
-      return { size, size};
+      return { size_, size_};
     }
 
     // Метод reshape
@@ -31,8 +31,55 @@ namespace ogxx
       if (new_shape.rows != new_shape.cols)
         throw std::invalid_argument("Symmetric matrix must have equal rows and cols.");
       
-      size = new_shape.rows;
-      data.resize(size * (size + 1) / 2, ST{});
+      size_ = new_shape.rows;
+      data_.resize(size * (size + 1) / 2, ST{});
+    }
+    auto iterate() const
+      -> Basic_iterator_uptr<ST> override
+    {
+    throw std::logic_error("Symmetric_dense_st_matrix::iterate not implemented.");
+    }
+
+    auto is_empty() const noexcept
+      -> bool override
+    {
+    throw std::logic_error("Symmetric_dense_st_matrix::is_empty not implemented.");
+    }
+    
+    auto size() const noexcept
+      -> Scalar_size override
+    {
+    throw std::logic_error("Symmetric_dense_st_matrix::size not implemented.");
+    }
+
+    auto iterate_row(Scalar_index row) const
+      -> Basic_iterator_uptr<ST> override
+    {
+    throw std::logic_error("Symmetric_dense_st_matrix::iterate_row not implemented.");
+    }
+
+    auto iterate_col(Scalar_index col) const
+      -> Basic_iterator_uptr<ST> override
+    {
+    throw std::logic_error("Symmetric_dense_st_matrix::iterate_col not implemented.");
+    }
+
+    auto view(Matrix_window window) const
+      -> St_matrix_const_uptr<ST> override
+    {
+    throw std::logic_error("Symmetric_dense_st_matrix::view not implemented.");
+    }
+
+    auto view(Matrix_window window)
+      -> St_matrix_uptr<ST> override
+    {
+    throw std::logic_error("Symmetric_dense_st_matrix::view not implemented.");
+    }
+
+    auto copy(Matrix_window window)
+      -> St_matrix_uptr<ST> override
+    {
+    throw std::logic_error("Symmetric_dense_st_matrix::view not implemented.");
     }
 
     // Метод get
@@ -44,21 +91,21 @@ namespace ogxx
       if (position.row > position.col)
         std::swap(position.row, position.col); // Убедитесь, что row <= col для симметрии
 
-      return data[position.row * size - (position.row - 1) * position.row / 2 + position.col - position.row];
+      return data_[position.row * size_ - (position.row - 1) * position.row / 2 + position.col - position.row];
     }
 
     // Метод set
     auto set(Matrix_index position, ST value) -> ST override
     {
-      if (position.row >= size || position.col >= size)
+      if (position.row >= size_ || position.col >= size_)
         throw std::out_of_range("Matrix position out of range.");
 
       if (position.row > position.col)
         std::swap(position.row, position.col); // Обеспечиваем row <= col для симметрии
 
-      auto index = position.row * size - (position.row - 1) * position.row / 2 + position.col - position.row;
-      auto old_value = data[index];
-      data[index] = value;
+      auto index = position.row * size_ - (position.row - 1) * position.row / 2 + position.col - position.row;
+      auto old_value = data_[index];
+      data_[index] = value;
       return old_value;
     }
 
@@ -72,7 +119,7 @@ namespace ogxx
     // Реализация метода заполнения
     void fill(ST value) noexcept override
     {
-      std::fill(data.begin(), data.end(), value);
+      std::fill(data_.begin(), data_.end(), value);
     }
   };
 }
--- tests/symmetric_dense_st_matrix_test.cpp
+++ tests/symmetric_dense_st_matrix_test.cpp
@@ -4,21 +4,13 @@
 #include "testing_head.hpp"
 #include <../source/symmetric_dense_st_matrix.cpp>
 
-/*
+
 TEST_SUITE("Initialization")
 {
   TEST_CASE("Initialization")
   {
-    Symmetric_dense_st_matrix A(5);
+    Symmetric_dense_st_matrix<int> A(5);
   }
 
  
-}
-
-*/
-namespace ogxx
-{
-int main(){
-    Symmetric_dense_st_matrix<int> A(5);
-}
 }
