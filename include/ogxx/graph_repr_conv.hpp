088938adcf6d649f75fd2d3ccb2384a89b45d23b
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-10-31 01:16:10 +0500

    Graph representation conversion
    Added (un)directed::convert::from(source_representation_object).to(target_representation_object)

---
+++ include/ogxx/graph_repr_conv.hpp
@@ -0,0 +1,138 @@
+/// @file graph_repr_conv.hpp
+/// @brief Graph representation conversion operations.
+/// @author Kuvshinov D.R. kuvshinovdr at yandex.ru
+/// Can we remove the duplication directed-undirected (the same-looking code calls different functions graph_view)?
+#ifndef OGXX_GRAPH_REPR_CONV_HPP_INCLUDED
+#define OGXX_GRAPH_REPR_CONV_HPP_INCLUDED
+
+#include "graph_view.hpp"
+#include "adjacency_list.hpp"
+#include "bit_matrix.hpp"
+#include "edge_list.hpp"
+
+
+/// Root namespace of the OGxx library.
+namespace ogxx
+{
+
+  /// Directed graph conversion.
+  namespace directed::convert
+  {
+
+    /// @brief A flow-interface implementation, holds "from" adapter graph view and does "to" directed graph conversion.
+    class Convertor
+    {
+    public:
+      /// @brief Initialize the "from" adapter graph view.
+      /// @param from graph view read-only object with ownership translation
+      Convertor(Graph_view_const_uptr from) noexcept
+        : _from(std::move(from)) {}
+
+      /// @brief Convert to an adjacency list.
+      /// @param al an adjacency list object to be filled
+      void to(Adjacency_list& al) const
+      {
+        graph_view(al)->copy_from(*_from);
+      }
+
+      /// @brief Convert to an adjacency matrix.
+      /// @param am a bit matrix representing the adjacency matrix to be filled
+      void to(Bit_matrix& am) const
+      {
+        graph_view(am)->copy_from(*_from);
+      }
+
+      /// @brief Convert to an edge list.
+      /// @param el an edge list object to be filled
+      void to(Edge_list& el) const
+      {
+        graph_view(el)->copy_from(*_from);
+      }
+
+    private:
+      Graph_view_const_uptr _from;
+    };
+
+
+    /// @brief Convert from an adjacency list to another representation.
+    /// @param al a reference to an adjacency list object
+    /// @return Convertor object (then use .to(target))
+    [[nodiscard]] inline auto from(Adjacency_list const& al)
+      -> Convertor { return { graph_view(al) }; }
+
+    /// @brief Convert from an adjacency matrix to another representation.
+    /// @param al a reference to an adjacency matrix (Bit_matrix) object
+    /// @return Convertor object (then use .to(target))
+    [[nodiscard]] inline auto from(Bit_matrix const& am)
+      -> Convertor { return { graph_view(am) }; }
+
+    /// @brief Convert from an edge list to another representation.
+    /// @param al a reference to an edge list object
+    /// @return Convertor object (then use .to(target))
+    [[nodiscard]] inline auto from(Edge_list const& el)
+      -> Convertor { return { graph_view(el) }; }
+
+  }
+
+  /// Undirected graph conversion.
+  namespace undirected::convert
+  {
+
+    /// @brief A flow-interface implementation, holds "from" adapter graph view and does "to" directed graph conversion.
+    class Convertor
+    {
+    public:
+      /// @brief Initialize the "from" adapter graph view.
+      /// @param from graph view read-only object with ownership translation
+      Convertor(Graph_view_const_uptr from) noexcept
+        : _from(std::move(from)) {}
+
+      /// @brief Convert to an adjacency list.
+      /// @param al an adjacency list object to be filled
+      void to(Adjacency_list& al) const
+      {
+        graph_view(al)->copy_from(*_from);
+      }
+
+      /// @brief Convert to an adjacency matrix.
+      /// @param am a bit matrix representing the adjacency matrix to be filled
+      void to(Bit_matrix& am) const
+      {
+        graph_view(am)->copy_from(*_from);
+      }
+
+      /// @brief Convert to an edge list.
+      /// @param el an edge list object to be filled
+      void to(Edge_list& el) const
+      {
+        graph_view(el)->copy_from(*_from);
+      }
+
+    private:
+      Graph_view_const_uptr _from;
+    };
+
+
+    /// @brief Convert from an adjacency list to another representation.
+    /// @param al a reference to an adjacency list object
+    /// @return Convertor object (then use .to(target))
+    [[nodiscard]] inline auto from(Adjacency_list const& al)
+      -> Convertor { return { graph_view(al) }; }
+
+    /// @brief Convert from an adjacency matrix to another representation.
+    /// @param al a reference to an adjacency matrix (Bit_matrix) object
+    /// @return Convertor object (then use .to(target))
+    [[nodiscard]] inline auto from(Bit_matrix const& am)
+      -> Convertor { return { graph_view(am) }; }
+
+    /// @brief Convert from an edge list to another representation.
+    /// @param al a reference to an edge list object
+    /// @return Convertor object (then use .to(target))
+    [[nodiscard]] inline auto from(Edge_list const& el)
+      -> Convertor { return { graph_view(el) }; }
+
+  }
+
+}
+
+#endif//OGXX_GRAPH_REPR_CONV_HPP_INCLUDED
