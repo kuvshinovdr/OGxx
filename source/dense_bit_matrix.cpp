6a7dab201b5d13622aeaa4b69872503a1d96f34c
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-11-02 17:01:00 +0500

    23.11.02

---
+++ source/dense_bit_matrix.cpp
@@ -0,0 +1,84 @@
+/// ... make file doc comment (file/brief/author)
+#include <vector>
+#include <ogxx/bit_matrix.hpp>
+
+#include <stdexcept>
+#include <climits>
+
+
+namespace ogxx {
+   class Dense_bit_matrix : public Bit_matrix{
+     public:
+       
+        // Возвращает размерность матрицы
+        Matrix_shape shape() override {
+            return _shape;
+        }
+
+        // Изменяет размерность матрицы
+        void reshape(Matrix_shape shape) override {
+            if (!shape.is_valid())
+              throw std::invalid_argument("Dense_bit_matrix::reshape: shape is invalid");
+            
+            auto const element_count = _shape.element_count();
+
+            // Изменяем размер вектора, но не заботимся о содержимом
+            bit_contain.resize((element_count + word_bits - 1) / word_bits);
+            _shape = shape;
+        }
+
+        // Получить значение бита по позиции
+        bool get(Matrix_index position)  override {
+            if (position.check_and_correct(_shape))
+            {
+                auto const bit_index   = position.row * _shape.cols + position.col;
+                auto const word_index  = bit_index / word_bits;
+                auto const bit_in_word = bit_index % word_bits;
+                return ((bit_contain[word_index] >> bit_in_word) & 1) == 1;
+            }
+            
+            return false;
+        }
+
+        // Установить значение бита по позиции
+        bool set(Matrix_index position, bool value) override {
+          if (position.check_and_correct(_shape))
+          {
+            auto const bit_index   = position.row * _shape.cols + position.col;
+            auto const word_index  = bit_index / word_bits;
+            auto const word        = bit_contain[word_index];
+            auto const bit_in_word = bit_index % word_bits;
+            bool const old         = ((word >> bit_in_word) & 1) == 1;
+            if (old != value)
+                bit_contain[word_index] = word ^ (Word(1) << bit_in_word);
+
+            return old;
+          }
+
+          throw std::out_of_range("Dense_bit_matrix::set: invalid position");
+        }
+
+        // Инвертировать значение бита по позиции
+        bool flip(Matrix_index position) override {
+            if(position.row > rows || position.col > cols) return false;
+            bit_contain[position.row * cols + position.col] = !data[position.row * cols + position.col];
+            return true;
+        }
+
+        // Заполнить всю матрицу указанным значением
+        void fill(bool value) override {
+            auto const fill_val = value? Word(-1): 0;
+            for (unsigned& word : bit_contain) {
+                word = fill_val;
+            }
+        }
+
+    private:
+        using Word = unsigned;
+        static constexpr auto word_bits = sizeof(Word) * CHAR_BIT;
+
+        std::vector<Word> bit_contain;
+        Matrix_shape _shape;
+   }
+}
+
