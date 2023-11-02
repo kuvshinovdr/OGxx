f080cc6ef5297f61474e77f8bdac5947da769bc3
KonstantinPM201 <K.S.Martianov@urfu.me>
2023-11-02 22:02:35 +0500

    Помог что-то починить.
    Надеюсь ничего не сломал.

--- source/index_set_hashtable.cpp
+++ source/index_set_hashtable.cpp
@@ -11,34 +11,43 @@
 #include <iostream>
 
 namespace ogxx{
-template <typename Scalar_index>    
-class Index_set_hashtable: public Index_set, public Sized_iterable<Scalar_index>
-{
+//template <typename Scalar_index>    
+    class Index_set_hashtable: public Index_set, public Sized_iterable<Scalar_index>{
     private:
         std::unordered_set<Scalar_index> unord_set;
     public:
         ~Index_set_hashtable() override {};
-        auto contains(Scalar_index item) override
-        {
-            return unord_set.contains(item);
-        }
-        auto insert(Scalar_index item) override
+        auto contains(Scalar_index item) const noexcept -> bool override {
+            return (unord_set.find(item) != unord_set.end());
+        } 
+        auto insert(Scalar_index item)
+            -> bool override
         {
             return unord_set.insert(item).second;
         }
-        auto erase(Scalar_index item) override
+        auto erase(Scalar_index item)
+            -> bool override
         {
             return unord_set.erase(item);
         }
-        auto size() override
+        auto size() const noexcept
+            -> Scalar_size override
         {
             return unord_set.size();
         }
-};
+        [[nodiscard]] auto iterate() const -> Basic_iterator_uptr<Scalar_index> override {
+            throw std::logic_error("iterator not implemented");
+        }
+
+        [[nodiscard]] auto is_empty() const noexcept
+        -> bool override {
+              throw std::logic_error("is_empty not implemented");
+        }
+    };
 }
 int main()
 {
-    ogxx::Index_set_hashtable<int> a;
+    ogxx::Index_set_hashtable a;
     std::cout<<5;    
 }
 #endif
