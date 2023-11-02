cc4cd4af2f32580387e590c1136c5333d39a358b
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-11-02 16:01:10 +0500

    Update edge_list_io_read.cpp

--- source/edge_list_io_read.cpp
+++ source/edge_list_io_read.cpp
@@ -4,13 +4,27 @@
 #ifndef OGXX_EDGE_LIST_IO_CPP_INCLUDED
 #define OGXX_EDGE_LIST_IO_CPP_INCLUDED
 
-#include "edge_list_io.hpp"
+#include <ogxx/edge_list_io.hpp>
+#include "parsing_utils.hpp"
+#include <iostream>
 
-auto read(std::istream& is, ogxx::Edge_list& el, Edge_list_format const& format)
--> std::istream&;
 
-auto read(ogxx::Edge_list& el, Edge_list_format const& format)
--> std::istream&;
+namespace ogxx::io
+{
 
+  auto read(std::istream& is, Edge_list& el, Edge_list_format const& format)
+    -> std::istream&
+  {
+    using namespace ogxx::io::util;
+
+  }
+
+  auto read(Edge_list& el, Edge_list_format const& format)
+    -> std::istream&
+  {
+    return read(std::cin, el, format);
+  }
+
+}
 
 #endif//OGXX_EDGE_LIST_IO_CPP_INCLUDED
