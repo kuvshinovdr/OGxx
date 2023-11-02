96423b5bfe94889293f0003692a404a609459e9c
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-11-03 02:07:26 +0500

    Update edge_list_io_read.cpp
    Updated to use string_view instead of istream.

--- source/edge_list_io_read.cpp
+++ source/edge_list_io_read.cpp
@@ -6,23 +6,16 @@
 
 #include <ogxx/edge_list_io.hpp>
 #include "parsing_utils.hpp"
-#include <iostream>
 
 
 namespace ogxx::io
 {
 
-  auto read(std::istream& is, Edge_list& el, Edge_list_format const& format)
-    -> std::istream&
+  auto read(std::string_view& in, Edge_list& el, Edge_list_format const& format)
+    -> bool
   {
     using namespace ogxx::io::util;
 
-  }
-
-  auto read(Edge_list& el, Edge_list_format const& format)
-    -> std::istream&
-  {
-    return read(std::cin, el, format);
   }
 
 }
