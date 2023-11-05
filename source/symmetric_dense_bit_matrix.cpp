c8c30b5cae3445a0ac520c0dc2f47e61931a3f09
Gilos03 <zbhegvsnz@gmail.com>
2023-11-05 12:29:37 +0500

    new version

--- source/symmetric_dense_bit_matrix.cpp
+++ source/symmetric_dense_bit_matrix.cpp
@@ -27,77 +27,108 @@ namespace ogxx {
             auto const element_count = shape.element_count();
 
             // Изменение размера вектора, но не заботясь о содержимом
-            bit_contain.resize((element_count  + word_bits -1) / word_bits);
+            // Теперь выделяем место только под нижнюю треугольную часть матрицы
+            bit_contain.resize((element_count - shape.rows) / 2 + shape.rows);
             _shape = shape;
         }
 
         // Получить значение бита по позиции
-        bool get(Matrix_index position) override{
-            if(position.check_and_correct(_shape)){
+        bool get(Matrix_index position) override {
+            if (position.check_and_correct(_shape)) {
                 auto const bit_index = position.row * _shape.cols + position.col;
-                if(position.row <= position.col) {
-                    auto const word_index = bit_index / word_bits;
-                    auto const bit_in_word = bit_index % word_bits;
-                    return ((bit.contain[word_index] >> bit_in_word) & 1) == 1;
-                }
+                auto const symmetric_index = position.col * _shape.cols + position.row;
+                auto const index = (position.row <= position.col) ? bit_index : symmetric_index;
 
-                else{
-                    // Доступ к симметричной части матрицы
-                    auto const symmetric_index = position.col * _shape.cols + position.row;
-                    auto const word_index = symmetric_index / word_bits;
-                    auto const bit_in_word = symmetric_index % word_bits;
-                    return ((bit_contain[word_index] >> bit_in_word) & 1) == 1;
-                }
+                auto const word_index = index / word_bits;
+                auto const bit_in_word = index % word_bits;
+                return ((bit_contain[word_index] >> bit_in_word) & 1) == 1;
             }
+
             return false;
         }
+
         // Установить значения бита по позиции
         bool set(Matrix_index position, bool value) override {
             if (position.check_and_correct(_shape)) {
                 auto const bit_index = position.row * _shape.cols + position.col;
-                auto const word_index = bit_index / word_bits;
-                auto const word = bit_contain[word_index];
+                auto const symmetric_index = position.col * _shape.cols + position.row;
+                auto const index = (position.row <= position.col) ? bit_index : symmetric_index;
+
+                auto const word_index = index / word_bits;
                 auto const bit_in_word = bit_index % word_bits;
 
-                if (position.row <= position.col) {
-                    bool const old = ((word >> bit_in_word) & 1) == 1;
-                    if (old != value)
-                        bit_contain[word_index] = word ^ (Word(1) << bit_in_word);
-                    return old;
-                } else {
-                    //Установить симметричную часть матрицы
-                    auto const symmetric_index = position.col * _shape.cols + position.row;
-                    auto const symmetric_word_index = symmetric_index / word_bits;
-                    auto const symmetric_bit_in_word = symmetric_index % word_bits;
-                    bool const old = ((bit_contain[symmetric_word_index] >> symmetric_bit_in_word) & 1) == 1;
-                    if (old != value)
-                        bit_contain[symmetric_word_index] = word ^ (Word(1) << symmetric_bit_in_word);
-                    return old;
-                }
+                bool const old = ((bit_contain[word_index] >> bit_in_word) & 1) == 1;
+                if (old != value)
+                    bit_contain[word_index] ^= (Word(1) << bit_in_word);
+                return old;
             }
+
             throw std::out_of_range("symmetric_dense_bit_matrix::set: invalid position")
         }
+
         // Инвертировать значение бита по позиции
-        bool flip(Matrix_index position) override{
+        bool flip(Matrix_index position) override {
             if (position.row > _shape.rows || position.col > _shape.cols) return false;
 
-            if (position.row <= position.col) {
-                bit_contain[position.row * _shape.cols + position.col] =
-                        !bit_contain[position.row * _shape.cols + position.col];
-            } else {
-                // Перевернуть симметричную часть матрицы
-                auto const symmetric_index = position.col * _shape.cols + position.row;
-                bit_contain[symmetric_index / word_bits] ^= (Word(1) << (symmetric_index % word_bits));
-            }
+            auto const bit_index = position.row * _shape.cols + position.col;
+            auto const symmetric_index = position.col * _shape.cols + position.row;
+            auto const index = (position.row <= position.col) ? bit_index : symmetric_index;
+
+            bit_contain[index / word_bits] ^= (Word(1) << (index % word_bits));
+
             return true;
         }
 
         // Заполнить всю(половину) матрицу указанным значением
         void fill(bool value) override {
             auto const fill_val = value ? Word(-1) : 0;
-            for (unsigned& word : bit_conrain) {
+            for (unsigned &word: bit_conrain) {
                 word = fill_val;
             }
+        }
+
+        //Копируем только нижнюю часть матрицы
+       auto copy(Matrix_window window) const
+       -> Bit_matrix_uptr override {
+
+            if (!window.is_valid() || window.start_row >= _shape.rows || window.start_col >= _shape.cols) {
+                throw std::invalid_argument("Symmetric_dense_bit_matrix::copy: invalid window.");
+            }
+
+            auto new_rows = std::min(window.rows, _shape.rows - window.start_row);
+            auto new_cols = std::min(window.cols, _shape.cols - window.start_col);
+
+            auto new_matrix = std::make_unique<Symmetric_dense_bit_matrix>();
+
+            new_matrix -> reshape({new_rows, new_cols});
+
+            for(Scalar_index i = 0; i < new_rows; i++) {
+                for(Scalar_index j = 0; j < new_cols; ++j) {
+                    auto value = get({window.start_row + i, window.start_col + j});
+                    new_matrix -> set({i, j}, value);
+                }
+            }
+           return new_matrix;
+        }
+
+        auto iterate_row(Scalar_index row) const
+        -> Basic_iterator_uptr<bool> override {
+            throw std::logic_error("Symmetric_dense_bit_matrix:iterate_row: not implemented.");
+        }
+
+        auto iterate_col(Scalar_index col) const
+        -> Basic_iterator_uptr<bool> override {
+            throw std::logic_error("Symmetric_dense_bit_matrix::iterate_col: not implemented.");
+        }
+
+        auto view(Matrix_window window) const
+        -> Basic_iterator_uptr<bool> override {
+            throw std::logic_error("Symmetric_dense_matrix::view: not implemented.")
+        }
+
+        auto view(Matrix_window window)
+        -> Basic_iterator_uptr override {
+            throw std::logic_error("Symmetric_dense_matrix::view: not implemented.")
         }
     private:
         using Word = unsigned;
