cadfc5825722facbea7d71dc432cc77eb874fc9c
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-11-06 16:46:15 +0500

    Update dense_st_matrix.cpp

--- source/dense_st_matrix.cpp
+++ source/dense_st_matrix.cpp
@@ -1,24 +1,13 @@
-﻿/// @file dense_matrix.cpp
+﻿/// @file dense_st_matrix.cpp
 /// @brief Dense matrix implementation (of abstract matrix interface from matrix.hpp).
-/// @author Kuvshinov D.R. kuvshinovdr at yandex.ru
-#include "matrix.hpp"
+/// @author 
+#include <ogxx/st_matrix.hpp>
+#include <ogxx/stl_iterator.hpp>
 #include <vector>
 #include <algorithm>//fill
 
 namespace ogxx
 {
-	template<typename ST>
-	struct Matrix_shape
-	{
-		ST rows;
-		ST columns;
-	};
-	template<typename ST>
-	struct Matrix_index
-	{
-		ST row;
-		ST column;
-	};
 
 	template<typename ST>
 	class Dense_st_matrix : public St_matrix<ST>
@@ -26,31 +15,86 @@ namespace ogxx
 	private:
 		Matrix_shape shape_;
 		std::vector<ST> matrix_;
+
 	public:
+		Dense_st_matrix(Dense_st_matrix const&) = default;
+		Dense_st_matrix(Dense_st_matrix&&)      = default;
+		Dense_st_matrix& operator=(Dense_st_matrix const&) = default;
+		Dense_st_matrix& operator=(Dense_st_matrix&&)      = default;
+
+		/// Создаёт пустую матрицу.
+		Dense_st_matrix() = default;
+
 		// Конструктор класса
-		Dense_st_matrix(Matrix_shape shape) {
+		explicit Dense_st_matrix(Matrix_shape shape) {
 			reshape(shape);
 		}
+
 		// Метод для получения размерности матрицы
-		Matrix_shape shape() const {
+		Matrix_shape shape() const override {
 			return shape_;
 		}
+
 		// Метод для изменения размерности матрицы
-		void reshape(Matrix_shape shape) {
+		void reshape(Matrix_shape shape) override {
+			matrix_.resize(shape.element_count());
 			shape_ = shape;
-			matrix_.resize(shape.rows * shape.columns);
 		}
+
+		Basic_iterator_uptr<Item> iterate() const override {
+			return new_stl_iterator(matrix_);
+		}
+
+		bool is_empty() const noexcept {
+			return matrix_.empty();
+		}
+
+		Scalar_size size() const noexcept {
+			return static_cast<Scalar_size>(matrix_.size());
+		}
+
 		// Метод для получения значения элемента матрицы по позиции
-		ST get(Matrix_index position) const {
-			return matrix_[position.row * shape_.columns + position.column];
+		ST get(Matrix_index position) const noexcept override {
+			if (position.check_and_correct(shape_))
+				return matrix_[position.row * shape_.cols + position.col];
+			return {};
 		}
+
 		// Метод для установки значения элемента матрицы по позиции
-		void set(Matrix_index position, ST value) {
-			matrix_[position.row * shape_.columns + position.column] = value;
+		void set(Matrix_index position, ST value) override {
+			if (position.check_and_correct(shape_))
+				matrix_[position.row * shape_.cols + position.col] = value;
+			else
+				throw std::out_of_range("Dense_st_matrix::set: invalid position");
 		}
+
 		// Метод для заполнения всей матрицы одним значением
-		void fill(ST value) {
+		void fill(ST value = {}) noexcept override {
 			std::fill(matrix_.begin(), matrix_.end(), value);
 		}
+
+		Basic_iterator_uptr<ST> iterate_row(Scalar_index row) const override {
+			throw std::logic_error("St_matrix::iterate_row: not implemented");
+		}
+
+		Basic_iterator_uptr<ST> iterate_col(Scalar_index col) const override {
+			throw std::logic_error("St_matrix::iterate_col: not implemented");
+		}
+
+		St_matrix_const_uptr<ST> view(Matrix_window window) const override {
+			throw std::logic_error("St_matrix::view(const): not implemented");
+		}
+
+		St_matrix_uptr<ST> view(Matrix_window window) override {
+			throw std::logic_error("St_matrix::view: not implemented");
+		}
+
+		St_matrix_uptr<ST> copy(Matrix_window window) override {
+			auto  result = std::make_unique<Dense_st_matrix<ST>>(window.shape);
+			auto& matrix = static_cast<Dense_st_matrix<ST>&>(*result);
+			// TODO: copy elements
+			return result;
+		}
 	};
+
 }
