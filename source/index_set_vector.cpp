060c08f7f0a1e73a328773f96746713cbe04e82b
kaigorodovMM <147080391+kaigorodovMM@users.noreply.github.com>
2023-11-08 18:56:37 +0500

    Update index_set_vector.cpp
    Первая версия файла

---
+++ source/index_set_vector.cpp
@@ -0,0 +1,17 @@
+/// @file index_set_vector.cpp
+/// @brief set interface (vector<Scalar_index>)
+/// @author KaigorodovAB
+
+include<source/stl_iterator.hpp>
+include<vector>
+
+namespace ogxx{
+	class Index_set_vector : public ogxx::Index_set, ogxx::Indexed_iterable<Scalar_index> {
+	private:
+		vector<Scalar_index> set;
+	public:
+		void push(Scalar_index element) {
+			set.push_back(element);
+		}
+	};
+}
