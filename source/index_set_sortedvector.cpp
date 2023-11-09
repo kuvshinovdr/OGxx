433b1be5c590e1ca4963594b2ea8b0eecba360e1
NasonenkoID <145911984+NasonenkoID@users.noreply.github.com>
2023-11-09 21:04:15 +0500

    Update index_set_sortedvector.cpp

--- source/index_set_sortedvector.cpp
+++ source/index_set_sortedvector.cpp
@@ -1,12 +1,8 @@
-/// add doc-comment (file, brief, author)
-
 #include <ogxx/st_set.hpp>
 #include <ogxx/iterable.hpp>
 #include <ogxx/stl_iterator.hpp>
-
 #include <vector>
 #include <algorithm>
-
 
 namespace ogxx {
     class Index_set_sortedvector : public Index_set, public Indexed_iterable<Scalar_index> {
@@ -14,40 +10,59 @@ namespace ogxx {
         std::vector<Scalar_index> sorted_vector;
         
     public:
-	// Поправьте все методы соответственно интерфейсам Index_set и Indexed_iterable<Scalar_index>
-	// добавьте ко всем методам override.
-        void insert(const Scalar_index& index) {
+        auto insert(Scalar_index index)
+        -> bool override {
             auto it = std::lower_bound(sorted_vector.begin(), sorted_vector.end(), index);
+            if(*it == index) return false;
             sorted_vector.insert(it, index);
+            return true;
         }
 
-        void erase(const Scalar_index& index) {
+        auto erase(Scalar_index index)
+        -> bool override {
             auto it = std::lower_bound(sorted_vector.begin(), sorted_vector.end(), index);
-            if (it != sorted_vector.end() && *it == index) {
-                sorted_vector.erase(it);
-            }
+            if (it == sorted_vector.end() || *it != index) return false;
+            sorted_vector.erase(it);
+            return true;
         }
 
-        bool contains(const Scalar_index& index) const {
+        auto contains(Scalar_index index) const noexcept
+        -> bool override {
             auto it = std::lower_bound(sorted_vector.begin(), sorted_vector.end(), index);
             return (it != sorted_vector.end() && *it == index);
         }
 
-        Scalar_index find(const Scalar_index& index) const {
-            auto it = std::lower_bound(sorted_vector.begin(), sorted_vector.end(), index);
-            if (it != sorted_vector.end() && *it == index) {
-                return *it;
-            }
-            throw std::out_of_range("Index not found");
+        [[nodiscard]] auto iterate() const
+        -> Basic_iterator_uptr<Scalar_index> override{
+            return new_stl_iterator(sorted_vector);
         }
 
-		// Нет, не так. См. Indexed_iterable.
-        using iterator = stl_iterator<typename std::vector<Scalar_index>::iterator>;
-        using const_iterator = stl_iterator<typename std::vector<Scalar_index>::const_iterator>;
+        [[nodiscard]] auto is_empty() const noexcept
+        -> bool override
+        {
+            return sorted_vector.empty();
+        }
 
-        iterator begin() { return iterator(sorted_vector.begin()); }
-        iterator end() { return iterator(sorted_vector.end()); }
-        const_iterator begin() const { return const_iterator(sorted_vector.begin()); }
-        const_iterator end() const { return const_iterator(sorted_vector.end()); }
-    };
+        [[nodiscard]] auto size() const noexcept
+        -> Scalar_size override
+        {
+            return sorted_vector.size();
+        }
+
+        [[nodiscard]] auto get(Scalar_index index) const
+        -> Scalar_index override
+        {
+            if(index > sorted_vector.size()) return 0;
+            return sorted_vector[index];
+        }
+
+        auto set(Scalar_index index, Scalar_index value)
+        -> Scalar_index override
+        {
+            if(index > sorted_vector.size()) return 0;
+            auto ret = sorted_vector[index];
+            sorted_vector[index] = value;
+            return ret;
+        }
+};
 }
