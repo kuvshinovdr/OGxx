e1fe8b2590e73d92a19bf94abfaed59c815794d2
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-11-09 15:53:55 +0500

    Adjacency_hashtable
    Junction point added for Adjacency and Index_set_hashtable.

--- source/index_set_hashtable.hpp
+++ source/index_set_hashtable.hpp
@@ -1,44 +1,64 @@
-/// @file index_set_hashtable.cpp
+/// @file index_set_hashtable.hpp
 /// @brief This file defines Index_set and Sized_iterable implementation based upon std::unordered_set
 /// @author ChernyshevMV
+#ifndef OGXX_INDEX_SET_HASHTABLE_HPP_INCLUDED
+#define OGXX_INDEX_SET_HASHTABLE_HPP_INCLUDED
+
 #include <ogxx/st_set.hpp>
 #include <ogxx/iterable.hpp>
 #include <ogxx/stl_iterator.hpp>
 
 #include <unordered_set>
 
-namespace ogxx{
+namespace ogxx
+{
 
-    class Index_set_hashtable: public Index_set, public Sized_iterable<Scalar_index>{
+    class Index_set_hashtable
+      : public virtual Index_set
+      , public virtual Sized_iterable<Scalar_index>
+    {
     private:
         std::unordered_set<Scalar_index> unord_set;
+
     public:
-        ~Index_set_hashtable() override {};
-        auto contains(Scalar_index item) const noexcept -> bool override {
+        Index_set_hashtable() = default;
+
+        [[nodiscard]] auto contains(Scalar_index item) const noexcept 
+          -> bool override
+        {
             return (unord_set.find(item) != unord_set.end());
         } 
+        
         auto insert(Scalar_index item)
             -> bool override
         {
             return unord_set.insert(item).second;
         }
+        
         auto erase(Scalar_index item)
             -> bool override
         {
             return unord_set.erase(item) != 0;
         }
-        auto size() const noexcept
-            -> Scalar_size override
+        
+        [[nodiscard]] auto size() const noexcept
+          -> Scalar_size override 
         {
             return unord_set.size();
         }
-        [[nodiscard]] auto iterate() const -> Basic_iterator_uptr<Scalar_index> override {
+
+        [[nodiscard]] auto iterate() const 
+          -> Basic_iterator_uptr<Scalar_index> override
+        {
             return new_stl_iterator(unord_set);
         }
 
         [[nodiscard]] auto is_empty() const noexcept
-        -> bool override {
+          -> bool override 
+        {
               return unord_set.empty();
         }
     };
 }
+
+#endif//OGXX_INDEX_SET_HASHTABLE_HPP_INCLUDED
