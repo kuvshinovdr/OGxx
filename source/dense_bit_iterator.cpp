cbf2f6fb09b31e6c8249b1c7fc10b1af824f8db1
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-11-16 15:31:41 +0500

    Update dense_bit_iterator.cpp

--- source/dense_bit_iterator.cpp
+++ source/dense_bit_iterator.cpp
@@ -12,19 +12,24 @@ namespace ogxx
 class Dense_bit_iterator : public Basic_iterator<bool> 
 {
 public:
-    Dense_bit_iterator(unsigned const* word, size_t firstbit, size_t endbit, size_t stride = 1)
-    : word(word), currentbit(firstbit), endbit(endbit), stride(stride) {}
-    ~Dense_bit_iterator() {}
+    Dense_bit_iterator(
+      unsigned const* word, 
+      size_t          firstbit, 
+      size_t          endbit, 
+      size_t          stride = 1
+    )
+      : word(word), currentbit(firstbit), endbit(endbit), stride(stride) {}
 
     /// @brief 'next' method
     bool next(bool& value) noexcept override {
         if (currentbit >= endbit)
           return false;
 
-        value = (word[currentbit / word_bits] >> (currentbit % word_bits)) & 1
+        value = (word[currentbit / word_bits] >> (currentbit % word_bits)) & 1;
         currentbit += stride;
         return true;
     }
+
 private:
     unsigned const* word;
     size_t currentbit;
