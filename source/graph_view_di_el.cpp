d4ba1912041c77650559a384fe754c01e8731924
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-11-16 18:06:58 +0500

    Update graph_view_di_el.cpp
    См. правки. Получится ли скомпилировать данный файл?

--- source/graph_view_di_el.cpp
+++ source/graph_view_di_el.cpp
@@ -1,5 +1,6 @@
-/// write doc comment (file/brief/author)
-/// graph_view_di_el.cpp/     /Abdullaeva Ainur
+/// @file graph_view_di_el.cpp
+/// @brief Graph view implementation for directed graph represented by an edge list.
+/// @author Abdullaeva Ainur
 #include <ogxx/graph_view.hpp>
 #include <ogxx/edge_list.hpp>
 
@@ -8,6 +9,7 @@
 
 namespace ogxx
 {
+
   class Graph_view_directed_edge_list : public Graph_view
   {
   public:
@@ -31,13 +33,13 @@ namespace ogxx
     [[nodiscard]] auto iterate_edges() const override
       -> Vertex_pair_iterator_uptr
     {
-        return std::make_unique<Vertex_pair_iterator_di_el>(_el);
+       _el.iterate();
     }
 
     [[nodiscard]] auto are_connected(Vertex_pair edge) const noexcept override
       -> bool
     {
-      return _el.find(Vertex_pair edge)
+      return _el.find(edge) != npos;
     }
 
 
@@ -57,11 +59,22 @@ namespace ogxx
     auto disconnect(Vertex_pair edge) override
       -> bool
     {
-      _
+      // Данный метод можно реализовать, если Edge_list на самом деле реализует List.
+      // Найти с помощью find и удалить по индексу с помощью take.
     }
 
   private:
     Edge_list & _el;
   };
+
+
+  namespace directed
+  {
+    auto graph_view(Edge_list& el)
+      -> Graph_view_uptr
+    {
+      return std::make_unique<Graph_view_directed_edge_list>(el);
+    }
+  }
 
 }
