4d81f2085915267ad2547f85dc552f488718a69c
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-11-16 16:49:38 +0500

    Update dense_bit_matrix.cpp

--- source/dense_bit_matrix.cpp
+++ source/dense_bit_matrix.cpp
@@ -1,17 +1,43 @@
 /// ... make file doc comment (file/brief/author)
-#include <vector>
 #include <ogxx/bit_matrix.hpp>
+#include <ogxx/stl_iterator.hpp>
 
-#include <stdexcept>
+#include <vector>
 #include <climits>
 
 
 namespace ogxx {
    class Dense_bit_matrix : public Bit_matrix{
      public:
+
+       Dense_bit_matrix() = default;
+
+       Dense_bit_matrix(Matrix_shape shape)
+       {
+          reshape(shape);
+       }
        
+       auto iterate() const
+         -> Bit_iterator_uptr override
+       {
+         return new_dense_bit_iterator(
+           bit_contain.data(), 0, bit_contain.size());
+       }
+
+       auto is_empty() const noexcept
+         -> bool
+       {
+         return bit_contain.empty();
+       }
+
+       auto size() const noexcept
+         -> Scalar_size override
+       {
+         return bit_contain.size() * word_bits;
+       }
+
         // Возвращает размерность матрицы
-        Matrix_shape shape() override {
+        Matrix_shape shape() const noexcept override {
             return _shape;
         }
 
@@ -28,10 +54,10 @@ namespace ogxx {
         }
 
         // Получить значение бита по позиции
-        bool get(Matrix_index position)  override {
-            if (position.check_and_correct(_shape))
+        bool get(Matrix_index position) const noexcept override {
+            if (_shape.check_and_correct(position))
             {
-                auto const bit_index   = position.linear_index(_shape);
+                auto const bit_index   = _shape.linear_index(position);
                 auto const word_index  = bit_index / word_bits;
                 auto const bit_in_word = bit_index % word_bits;
                 return ((bit_contain[word_index] >> bit_in_word) & 1) == 1;
@@ -42,9 +68,9 @@ namespace ogxx {
 
         // Установить значение бита по позиции
         bool set(Matrix_index position, bool value) override {
-          if (position.check_and_correct(_shape))
+          if (_shape.check_and_correct(position))
           {
-            auto const bit_index   = position.linear_index(_shape);
+            auto const bit_index   = _shape.linear_index(position);
             auto const word_index  = bit_index / word_bits;
             auto const word        = bit_contain[word_index];
             auto const bit_in_word = bit_index % word_bits;
@@ -60,21 +86,22 @@ namespace ogxx {
 
         // Инвертировать значение бита по позиции
         bool flip(Matrix_index position) override {
-			if (position.check_and_correct(_shape)) {
-				auto const bit_index   = position.linear_index(_shape);
-				auto const word_index  = bit_index / word_bits;
-				auto const word        = bit_contain[word_index];
-				auto const bit_in_word = bit_index % word_bits;
-				bool const old = ((word >> bit_in_word) & 1) == 1;
+			    if (_shape.check_and_correct(position))
+          {
+				    auto const bit_index   = _shape.linear_index(position);
+				    auto const word_index  = bit_index / word_bits;
+				    auto const word        = bit_contain[word_index];
+				    auto const bit_in_word = bit_index % word_bits;
+				    bool const old = ((word >> bit_in_word) & 1) == 1;
 
-				// Инвертировать значение бита
-				bit_contain[word_index] = word ^ (Word(1) << bit_in_word);
+				    // Инвертировать значение бита
+				    bit_contain[word_index] = word ^ (Word(1) << bit_in_word);
 
-				return old;
-			}
+				    return old;
+			    }
 
-			throw std::out_of_range("Dense_bit_matrix::flip: invalid position");
-		}
+			    throw std::out_of_range("Dense_bit_matrix::flip: invalid position");
+		    }
 
             
 
@@ -87,31 +114,56 @@ namespace ogxx {
         }
 		
         //временная заглушка функции
-        auto iterate_row(Scalar_index row)override {
-        auto iterator = std::make_unique<bool>(true);
-        return iterator;
+        auto iterate_row(Scalar_index row) const
+          -> Basic_iterator_uptr<bool> override 
+        {
+          throw std::logic_error("Dense_bit_matrix::iterate_row: not implemented");
         }
+
          //временная заглушка функции
-        auto iterate_col(Scalar_index col)override {
-        auto iterator = std::make_unique<bool>(true);
-        return iterator;
+        auto iterate_col(Scalar_index col) const
+          -> Basic_iterator_uptr<bool> override
+        {
+          throw std::logic_error("Dense_bit_matrix::iterate_col: not implemented");
         }
-        auto view(Matrix_window window) override {
-        
-        auto bitMatrix = std::make_unique<Bit_matrix_const>();
-        return bitMatrix;
+
+        auto view(Matrix_window window) const
+          -> Bit_matrix_const_uptr override 
+        {
+          throw std::logic_error("Dense_bit_matrix::view: not implemented");
+        }
+
+        auto view(Matrix_window window)
+          -> Bit_matrix_uptr override
+        {
+          throw std::logic_error("Dense_bit_matrix::view: not implemented");
+        }
+
+        auto copy(Matrix_window window) const
+          -> Bit_matrix_uptr override
+        {
+          if (!window.fits_into(_shape))
+            throw std::out_of_range("Dense_bit_matrix::copy: window does not fit into the shape of the matrix");
+
+          auto copy = new_dense_bit_matrix(window.shape);
+
+          return copy;
         }
         
-};
-
-
-       
     private:
         using Word = unsigned;
         static constexpr auto word_bits = sizeof(Word) * CHAR_BIT;
 
         std::vector<Word> bit_contain;
         Matrix_shape _shape;
+   };
+
+
+   auto new_dense_bit_matrix(Matrix_shape shape)
+     -> Bit_matrix_uptr
+   {
+     return std::make_unique<Dense_bit_matrix>(shape);
    }
+
 }
 
