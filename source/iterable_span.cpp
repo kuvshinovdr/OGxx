2964bc7ed8084c8351c44a2be793ea3fc661fe42
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-11-23 16:50:58 +0500

    Update iterable_span.cpp

--- source/iterable_span.cpp
+++ source/iterable_span.cpp
@@ -1,8 +1,58 @@
 /// doc-comment
 #include <ogxx/iterable.hpp>
+#include <ogxx/stl_iterator.hpp>
+#include <iterator>
 
 namespace ogxx
 {
+
+  namespace
+  {
+    template <typename Item>
+    class Iterable_span_iterator
+    {
+    public:
+      Iterable_span_iterator() noexcept = default;
+      Iterable_span_iterator(Pass_by<Item>* at, Scalar_index stride = 1)
+        : _cur(at), _stride(stride) {}
+
+      auto operator*() const noexcept
+        -> Pass_by<Item>& { return *_cur; }
+
+      auto operator->() const noexcept
+        -> Pass_by<Item>* { return _cur; }
+
+      auto operator++() noexcept
+        -> Iterable_span_iterator& 
+      { 
+        _cur += _stride;
+        return *this;
+      }
+
+      auto operator++(int) noexcept
+        -> Iterable_span_iterator
+      {
+        auto old = *this;
+        ++*this;
+        return old;
+      }
+
+      friend auto operator==(
+        Iterable_span_iterator<Item> a, 
+        Iterable_span_iterator<Item> b) noexcept
+        -> bool { return a._cur == b._cur; }
+
+    private:
+      Pass_by<Item>* _cur    = nullptr;
+      Scalar_index   _stride = 1;
+    };
+
+    inline auto operator!=(
+      Iterable_span_iterator<Item> a,
+      Iterable_span_iterator<Item> b) noexcept
+      -> bool { return !(a == b); }
+  }
+
   
   template <typename Item>
   class Iterable_span
@@ -11,13 +61,18 @@ namespace ogxx
   public:
     Iterable_span() = default;
 
-    Iterable_span(Item *begin, Item *end, Scalar_index stride = 1)
+    Iterable_span(
+      Pass_by<Item> *begin, 
+      Pass_by<Item> *end, 
+      Scalar_index stride = 1)
       : _begin(begin), _end(end), _stride(stride) {}
 
     [[nodiscard]] auto iterate() const
       -> Basic_iterator_uptr<See_by<Item>> override
     {
-      // TODO
+      return new_stl_iterator(
+        Iterable_span_iterator(_begin, _stride),
+        Iterable_span_iterator(_end, _stride));
     }
 
     [[nodiscard]] auto is_empty() const noexcept
@@ -39,22 +94,29 @@ namespace ogxx
       if (!is_within(ptr, _begin, _end))
         throw std::out_of_range("Iterable_span::get: invalid index");
       
-      if constexpr (std::is_pointer_v<See_by<Item>>)
-        return ptr;
-      else
+      if constexpr (std::is_pointer_v<See_by<Item>>) // abstract class
+        return ptr->get();
+      else // non-abstract
         return *ptr; // only if See_by<Item> === Item
     }
 
     [[nodiscard]] auto set(Scalar_index index, Pass_by<Item> value)
       -> Pass_by<Item> override
     {
+      auto const ptr = _begin + _stride * index;
+      if (!is_within(ptr, _begin, _end))
+        throw std::out_of_range("Iterable_span::get: invalid index");
+
       // TODO
+      // save old *ptr (old = move(*ptr))
+      // move value into *ptr
+      // return old
     }
 
   private:
-    Item *_begin = nullptr;
-    Item *_end   = nullptr;
-    Scalar_index _stride = 1;
+    Pass_by<Item> *_begin = nullptr;
+    Pass_by<Item> *_end   = nullptr;
+    Scalar_index _stride  = 1;
   };
 
 }
