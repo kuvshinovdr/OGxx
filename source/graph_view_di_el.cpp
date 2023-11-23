9e7f059e44d8d3101bf449fbce05009ebc9bc6c5
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-11-23 17:10:34 +0500

    Graph_view_di_el accepted

--- source/graph_view_di_el.cpp
+++ source/graph_view_di_el.cpp
@@ -3,6 +3,7 @@
 /// @author Abdullaeva Ainur
 #include <ogxx/graph_view.hpp>
 #include <ogxx/edge_list.hpp>
+#include <ogxx/iterable.hpp>
 
 #include <stdexcept>
 
@@ -18,49 +19,60 @@ namespace ogxx
 
     // Constant interface
 
-    [[nodiscard]] auto vertex_count() const noexcept override
-      -> Scalar_size
+    [[nodiscard]] auto is_directed() const noexcept
+      -> bool override { return true; }
+
+    [[nodiscard]] auto vertex_count() const noexcept
+      -> Scalar_size                        override
     {
       return _el.max_vertex_index() + 1;
     }
 
-    [[nodiscard]] auto edge_count() const noexcept override
-      -> Scalar_size
+    [[nodiscard]] auto edge_count() const noexcept
+      -> Scalar_size                      override
     {
         return _el.size();
     }
 
-    [[nodiscard]] auto iterate_edges() const override
-      -> Vertex_pair_iterator_uptr
+    [[nodiscard]] auto iterate_edges() const
+      -> Vertex_pair_iterator_uptr     override
     {
-       _el.iterate();
+       return _el.iterate();
     }
 
-    [[nodiscard]] auto are_connected(Vertex_pair edge) const noexcept override
-      -> bool
-    {
-      return _el.find(edge) != npos;
-    }
+    [[nodiscard]] auto are_connected(Vertex_pair edge) const noexcept
+      -> bool override { return _el.find(edge) != npos; }
 
 
     // Non-constant interface
 
     void set_vertex_count(Scalar_size count) override
     {
-      // Pending solution.
-      throw std::invalid_operation("Graph_view_directed_edge_list::set_vertex_count: not implemented");
+      // Pending solution: now ignoring this call.
+      //throw std::logic_error("Graph_view_directed_edge_list::set_vertex_count: not implemented");
     }
 
-    auto connect(Vertex_pair edge) override
-    {
+    auto connect(Vertex_pair edge)
+      -> bool override 
+    { 
       _el.put(edge);
+      return true;
     }
 
-    auto disconnect(Vertex_pair edge) override
-      -> bool
+    auto disconnect(Vertex_pair edge)
+      -> bool override
     {
-      // Данный метод можно реализовать, если Edge_list на самом деле реализует List.
-      // Найти с помощью find и удалить по индексу с помощью take.
+      if (auto vp_list_ptr = dynamic_cast<List<Vertex_pair>*>(&_el))
+      {
+        auto const pos = _el.find(edge);
+        if (pos == npos)
+          return false;
+
+        vp_list_ptr->take(pos);
+        return true;
+      }
+
+      throw std::logic_error("Graph_view_directed_edge_list::disconnect: the attached edge list does not support List operations");
     }
 
   private:
