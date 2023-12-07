8ba3413418313c83a5d9a9d78f92cd4bc9a9e9ee
MansurovSh <147724618+MansurovSh@users.noreply.github.com>
2023-12-07 17:22:47 +0500

    Addes bit_matrix_io_prin

---
+++ tests/bit_matrix_io_print.cpp
@@ -0,0 +1,21 @@
+/// @file bit_matrix_io_print.cpp
+/// @brief test for print bit matrix
+/// @author Mansurov Sh.
+#include "testing_head.hpp"
+//#include <source/dense_bit_matrix.cpp>
+#include <ogxx/bit_matrix_io.hpp>
+#include <sstream>
+
+TEST_SUITE("print")
+{
+  TEST_CASE("print")
+  {
+    Matrix_shape _shape = {2, 3};
+    Bit_matrix_uptr bm = new_dense_bit_matrix(_shape);
+    bm->fill(1);
+    bm->set(1,1,0);
+    std::ostringstream os;
+    io::print(os, *bm);
+    CHECK(os.str() == "11" );
+  }
+}
