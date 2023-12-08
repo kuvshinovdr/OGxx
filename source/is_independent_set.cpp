ca7f3b636419d368ba4b612c339560460aaff9c9
Krukovich Sonya <146754132+soffiku@users.noreply.github.com>
2023-12-08 18:50:20 +0500

    Create is_independent_set.cpp

---
+++ source/is_independent_set.cpp
@@ -0,0 +1,6 @@
+namespace ogxx::directed {
+ auto is_independent_set(Graph_view const& gv, Index_iterator_uptr vertices) -> bool;
+}
+namespace ogxx::undirected {
+ auto is_independent_set(Graph_view const& gv, Index_iterator_uptr vertices) -> bool;
+}
