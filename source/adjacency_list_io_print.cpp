0e3e5912b4447c1b3c918a45bf81ee7066461d10
Nikolai-Fedorov-PM-201 <146085274+Nikolai-Fedorov-PM-201@users.noreply.github.com>
2023-12-13 17:59:32 +0500

    Update adjacency_list_io_print.cpp

--- source/adjacency_list_io_print.cpp
+++ source/adjacency_list_io_print.cpp
@@ -1,17 +1,22 @@
 #include "ogxx/adjacency_list_io.hpp"
 
-void ogxx::io::print(std::ostream& ostr, ogxx::Adjacency_list const& adj_list) {
+void ogxx::io::print(std::ostream& ostr, ogxx::Adjacency_list const& adj_list, Adjacency_list_format const& format = {}) {
     auto sz = adj_list.size();
+
+    ostr << format.list_open;
 
     for (Scalar_index vertex_ind = 0; vertex_ind < sz; ++vertex_ind) {
         auto const neighbours = adj_list.get(vertex_ind);
 
-        ostr << "Neighbours for vertex " << vertex_ind << ": ";
+        ostr << vertex_ind << format.adjacency_open;
 
         auto iterator = neighbours->iterate();
         for (Scalar_index neighbour_index; iterator->next(neighbour_index);) {
-            ostr << neighbour_index <<  ", ";
+            ostr << neighbour_index <<  format.adjacency_sep;
         }
-        ostr << "\n";
+
+        ostr << format.adjacency_close;
     }
+
+    ostr << format.list_close;
 }
