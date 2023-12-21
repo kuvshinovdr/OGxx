6d44b3b57386b38e63bb3ce9284584d8e0c7da59
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-12-21 16:15:50 +0500

    Update bit_matrix_io_print.cpp

--- tests/bit_matrix_io_print.cpp
+++ tests/bit_matrix_io_print.cpp
@@ -16,12 +16,11 @@ TEST_SUITE("print")
     bm->set(1,1,0);
     std::ostringstream os;
     io::print(os, *bm);
-    CHECK(os.str() == "bit_matrix
-    {
-      0100 01
-      1011 00
-      0110 01
-      }
-      " );
+    CHECK(os.str() == "bit_matrix\
+    {\
+      0100 01\
+      1011 00\
+      0110 01\
+      }" );
   }
 }
