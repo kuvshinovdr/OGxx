369535538e2bae0e15e503e401d39a49dcb22bfb
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-12-21 18:05:18 +0500

    Update random_choice_bag.cpp
    Fixed.

--- source/random_choice_bag.cpp
+++ source/random_choice_bag.cpp
@@ -1,9 +1,13 @@
 /// @file Random_choice_bag #73.cpp
 /// @brief the Random_choice_bag<T> class, which implements the Bag<T> interface and allows items to be retrieved in random order
 /// @author Sutyagin A.I., nickname Alexanders2007, email asutagin062@gmail.com
+#include <ogxx/iterable.hpp>
+#include <ogxx/stl_iterator.hpp>
 #include <vector>
 #include <random>
-#include <ogxx/iterable.hpp>
+
+namespace ogxx
+{
 
 template<typename T>
 class Random_choice_bag : public Bag<T> {
@@ -15,13 +19,20 @@ public:
     Random_choice_bag() // Инициализация генератора случайных чисел
       : random_engine(std::random_device{}()) {}
 
+    explicit Random_choice_bag(Index_iterator_uptr items)
+      : Random_choice_bag()
+    {
+        for (Scalar_index value; items->next(value);)
+            elements.push_back(value);
+    }
+
     void put(Pass_by<T> element) override {
         elements.push_back(element);
     }
 
     Pass_by<T> take() override {
         if (elements.empty()) {
-            throw std::runtime_error("Bag is empty");
+            throw std::runtime_error("Random_choice_bag::take: the bag is empty");
         }
 
         // Генерация случайного индекса
@@ -38,23 +49,38 @@ public:
         return removed_element;
     }
 
+    void clear() override
+    {
+        elements.clear();
+    }
+
     auto iterate() const
-      ->Basic_iterator_uptr<See_by<T>> override
+      -> Basic_iterator_uptr<See_by<T>> override
     {  
-      using IteratorType = typename std::vector<Pass_by<T>>::const_iterator; 
-      auto iterator = std::make_unique<STL_iterator<IteratorType>>(elements.begin(), elements.end()); 
-      return iterator; 
+        return new_stl_iterator(elements);
     }
 
     auto is_empty() const noexcept
       -> bool override
     {
-      return elements.empty();
+        return elements.empty();
     }
 
     auto size() const noexcept
       -> Scalar_size override
     {
-      return elements.size();
+        return elements.size();
     }
 };
+
+auto new_index_random_choice_bag() -> Index_bag_uptr
+{
+  return std::make_unique<Random_choice_bag<Scalar_index>>();
+}
+
+auto new_index_random_choice_bag(Index_iterator_uptr items) -> Index_bag_uptr
+{
+  return std::make_unique<Random_choice_bag<Scalar_index>>(std::move(items));
+}
+
+}
