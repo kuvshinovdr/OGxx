8012f44594b0f9fa767310115c30f3c14e500344
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-12-26 14:01:54 +0500

    Update Stack-Queue-Mixed_stack_queue.cpp
    1. Нет нужды наследовать виртуально, ведь данные классы не предназначены быть базовыми для других.
    2. Значения, обёрнутые в Pass_by, надо перемещать, копирование может быть недоступно (unique_ptr).
    3. Реализация take в Mixed_stack_queue была очень медленной, я перенёс генератор и распределение в private-часть, а их инициализацию -- в конструктор.

--- source/Stack-Queue-Mixed_stack_queue.cpp
+++ source/Stack-Queue-Mixed_stack_queue.cpp
@@ -9,7 +9,7 @@
 
 namespace ogxx{
     template <typename T>
-    class Stack : public virtual Bag<T> {
+    class Stack : public Bag<T> {
     private:
         std::deque<Pass_by<T>> data;
 
@@ -20,7 +20,7 @@ namespace ogxx{
 
         auto take() -> Pass_by<T> override {
             if (!this->is_empty()) {
-                Pass_by<T> item = data.back();
+                Pass_by<T> item = std::move(data.back());
                 data.pop_back();
                 return item;
             }
@@ -33,7 +33,7 @@ namespace ogxx{
     };
 
     template <typename T>
-    class Queue : public virtual Bag<T> {
+    class Queue : public Bag<T> {
     private:
         std::deque<Pass_by<T>> data;
 
@@ -44,7 +44,7 @@ namespace ogxx{
 
         auto take() -> Pass_by<T> override {
             if (!this->is_empty()) {
-                Pass_by<T> item = data.front();
+                Pass_by<T> item = std::move(data.front());
                 data.pop_front();
                 return item;
             }
@@ -57,13 +57,15 @@ namespace ogxx{
     };
 
     template <typename T>
-    class Mixed_stack_queue : public virtual Bag<T> {
+    class Mixed_stack_queue : public Bag<T> {
     private:
-        std::deque<Pass_by<T>> data;
-        double p; // Вероятность использования стека
+        std::deque<Pass_by<T>>       data;
+		std::default_random_engine   rng;
+		std::bernoulli_distribution  distr;
 
     public:
-        Mixed_stack_queue(double probability) : p(probability) {}
+        explicit Mixed_stack_queue(double probability = 0.5) 
+		    : distr(probability), rng(std::random_device{}()) {}
 
         void put(Pass_by<T> item) override {
             data.push_back(item);
@@ -71,16 +73,12 @@ namespace ogxx{
 
         auto take() -> Pass_by<T> override {
             if (!this->is_empty()) {
-                std::random_device rd;
-                std::default_random_engine generator(rd());
-                std::bernoulli_distribution distribution(p);
-
-                if (distribution(generator)) {
-                    Pass_by<T> item = data.back();
+                if (distr(rng)) {
+                    Pass_by<T> item = std::move(data.back());
                     data.pop_back();
                     return item;
                 } else {
-                    Pass_by<T> item = data.front();
+                    Pass_by<T> item = std::move(data.front());
                     data.pop_front();
                     return item;
                 }
