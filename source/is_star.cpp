e0045b0447e7b0e6bd5312324a31ec16e37cef2b
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2023-12-28 14:48:10 +0500

    Update is_star.cpp
    Fixes.

--- source/is_star.cpp
+++ source/is_star.cpp
@@ -8,25 +8,27 @@
 
 namespace ogxx
 {
-    auto get_deg_vertex(Vertex_index index, std::vector<Vertex_index>& vertex, int len, Graph_view const& gv) { // Get the degree of the vertex
-        int deg=0;
-        for (int j = 0; j < len; j++) {
-            if (gv.are_connected(*vertex[index], *vertex[j])) {
+    auto get_deg_vertex(Vertex_index index, std::vector<Vertex_index>& vertex, Graph_view const& gv) { // Get the degree of the vertex
+        size_t deg=0;
+        for (auto v: vertex) {
+            if (gv.are_connected(index, v)) {
                 deg++;
             }
         }
         return deg;
     }
+	
     auto is_star(Index_iterator_uptr vertices, Graph_view const& gv)
         -> std::optional<Vertex_index> {
         std::vector <Vertex_index> vertex;
         for (Vertex_index new; vertices->next(new)) {
             vertex.emplace_back(new);
         }
-        int len = vertex.size();
+        auto const len = vertex.size();
         if (len < 3)
             return {};
-        int sum = 0, center = -1;
+        size_t sum = 0;
+		Vertex_index center = -1;
         for (auto index : vertex) {
             auto deg = get_deg_vertex(index, vertex, len, gv);
             sum += deg;
