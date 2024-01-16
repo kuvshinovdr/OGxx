68999c1445b950d1040d9f4bc9539a422773ef47
Dmitry R. Kuvshinov <135211050+kuvshinovdr@users.noreply.github.com>
2024-01-16 14:02:54 +0500

    Update edge_list_io_read.cpp
    Removed cpp includes. Fixed to compile properly.

--- tests/edge_list_io_read.cpp
+++ tests/edge_list_io_read.cpp
@@ -2,10 +2,8 @@
 /// @brief Test of ogxx::io::read from 'edge_list_io_read.cpp'
 /// @author Chaplygin M.D.
 
-#include "tests/testing_head.hpp"
-#include <source/edge_list_vector.cpp>
+#include "testing_head.hpp"
 #include <ogxx/edge_list_io.hpp>
-#include <edge_list_io_read.cpp>
 
 #include <string>
 #include <string_view>
@@ -18,7 +16,11 @@ namespace ogxx
 	{
 		TEST_CASE("read")
 		{
-			Edge_list_vector el1, el2, el3;
+			auto 
+				el1 = new_edge_list_vector(), 
+				el2 = new_edge_list_vector(),
+				el3 = new_edge_list_vector();
+
 			io::Edge_list_format format;
 
 			std::string_view test_in1 = "edge_list\n{\n  (1, 2)\n  (2, 3)\n  (3, 4)\n  (4, 5)\n  (5, 6)\n}\n",
@@ -28,12 +30,12 @@ namespace ogxx
 											 test_in5 = "{\n(1,2)\n(2,3)\n(3,4)\n(4,5)\n(5,6)\n}\n",
 											 test_in6 = "ege_list\n{\n(1,2)\n(2,3)\n(3,4)\n(4,5)\n(5,6)\n}\n";
 
-			CHECK(io::read(test_in1, el1, format));
-			CHECK(io::read(test_in2, el2, format));
-			CHECK(io::read(test_in3, el3, format));
-			CHECK(io::read(test_in4, el3, format));
-			CHECK(!io::read(test_in5, el3, format));
-			CHECK(!io::read(test_in6, el3, format));
+			CHECK(io::read(test_in1, *el1, format));
+			CHECK(io::read(test_in2, *el2, format));
+			CHECK(io::read(test_in3, *el3, format));
+			CHECK(io::read(test_in4, *el3, format));
+			CHECK(!io::read(test_in5, *el3, format));
+			CHECK(!io::read(test_in6, *el3, format));
 		}
 	}
 }
