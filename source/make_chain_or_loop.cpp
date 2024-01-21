32b66cac4697911329ded8706e8a72dedb27165a
Anuarch1k <143879565+Anuarch1k@users.noreply.github.com>
2024-01-21 20:10:27 +0500

    Add files via upload

--- source/make_chain_or_loop.cpp
+++ source/make_chain_or_loop.cpp
@@ -3,27 +3,28 @@
 
 namespace ogxx {
 
-    
+
     void make_chain(Graph_view& gv, Index_iterator_uptr vertices) {
         int v1, v2;
-        if (vertices->next(v1)) {  
+        if (vertices->next(v1)) {
             while (vertices->next(v2)) {
-                gv.connect(v1, v2);  
+                gv.connect(v1, v2);
                 v1 = v2;
             }
         }
     }
 
-    
+
     void make_loop(Graph_view& gv, Index_iterator_uptr vertices) {
         int v1, v2;
-        if (vertices->next(v1)) {  
+        if (vertices->next(v1)) {
+            int firstVertex = v1;
             while (vertices->next(v2)) {
-                gv.connect(v1, v2);  
+                gv.connect(v1, v2);
                 v1 = v2;
             }
-            gv.connect(v1, vertices->first());
+            gv.connect(v1, firstVertex);
         }
     }
 
-}  
+}
