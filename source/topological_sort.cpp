4245aee105cab54cf9bbc7da8c6b5c2ae5e39b18
Anuarch1k <143879565+Anuarch1k@users.noreply.github.com>
2024-01-21 19:52:54 +0500

    Add files via upload

--- source/topological_sort.cpp
+++ source/topological_sort.cpp
@@ -7,24 +7,31 @@ namespace ogxx {
 
     Index_iterator_uptr topological_sort(Graph_view& gv) {
         std::vector<int> in_degree(gv.num_vertices(), 0);
-        for (int v = 0; v < gv.num_vertices(); ++v) {
+        for (Vertex_index v = 0; v < gv.num_vertices(); ++v) {
             for (const auto& neighbor : gv.neighbors(v)) {
                 in_degree[neighbor]++;
             }
         }
-        std::queue<int> zero_in_degree_vertices;
-        for (int v = 0; v < gv.num_vertices(); ++v) {
+
+        std::queue<Vertex_index> zero_in_degree_vertices;
+        for (Vertex_index v = 0; v < gv.num_vertices(); ++v) {
             if (in_degree[v] == 0) {
                 zero_in_degree_vertices.push(v);
             }
         }
-        std::vector<int> topological_order;
+
+        std::vector<Vertex_index> topological_order;
         while (!zero_in_degree_vertices.empty()) {
-            int u = zero_in_degree_vertices.front();
+            Vertex_index u = zero_in_degree_vertices.front();
             zero_in_degree_vertices.pop();
             topological_order.push_back(u);
 
+            Vertex_index first_vertex = -1;
             for (const auto& neighbor : gv.neighbors(u)) {
+                if (first_vertex == -1) {
+                    first_vertex = neighbor;
+                }
+
                 if (--in_degree[neighbor] == 0) {
                     zero_in_degree_vertices.push(neighbor);
                 }
@@ -36,7 +43,7 @@ namespace ogxx {
         }
 
         auto result = std::make_unique<Index_iterator>();
-        for (int v : topological_order) {
+        for (Vertex_index v : topological_order) {
             result->add(v);
         }
 
